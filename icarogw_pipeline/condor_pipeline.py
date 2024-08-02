ff218f968ba50b00ad24e832d2a1f7249b4bc60a
Vasco Gennari <vasco.gennari@gmail.com>
2024-08-02 18:16:34 +0200

    Minors condor pipeline

--- icarogw_pipeline/condor_pipeline.py
+++ icarogw_pipeline/condor_pipeline.py
@@ -6,8 +6,8 @@ arguments = "{script} --config-file {config}"
 request_cpus = {cpus}
 request_memory = {memory} GB
 request_disk = {disk} GB
-log = condor_files/{log_name}.log
-error = condor_files/{log_name}.err
+log = {condor_path}/{log_name}.log
+error = {condor_path}/{log_name}.err
 accounting_group = ligo.prod.o4.cbc.hubble.icarogw
 accounting_group_user = {user_name}
 getenv = True
@@ -16,19 +16,20 @@ queue"""
 
 def activate_condor_submit(config_name):
 
-    subfile = 'submission_files/submit_icarogw_{}.sub'.format(config_name.split('/')[-1].split('.ini')[0].split('config_')[-1])
+    subfile = '{}/submit_icarogw_{}.sub'.format(sub_path, config_name.split('/')[-1].split('.ini')[0].split('config_')[-1])
     sys.stderr.write('generating {}\n'.format(subfile))
 
     with open(subfile,'w') as f:
 
-        submission_command = template.format(executable = condor_executable_path,
-                                             script     = condor_executable_file,
-                                             config     = config_name,
-                                             log_name   = config_name.split('/')[-1].split('.ini')[0].split('config_')[-1],
-                                             cpus       = condor_threads,
-                                             memory     = condor_memory,
-                                             disk       = condor_disk,
-                                             user_name  = user_name)
+        submission_command = template.format(executable  = condor_executable_path,
+                                             script      = condor_executable_file,
+                                             config      = config_name,
+                                             condor_path = con_path,
+                                             log_name    = config_name.split('/')[-1].split('.ini')[0].split('config_')[-1],
+                                             cpus        = condor_threads,
+                                             memory      = condor_memory,
+                                             disk        = condor_disk,
+                                             user_name   = user_name)
                                              
         f.write(submission_command)
     sys.stderr.write('submitting {}\n'.format(subfile))
@@ -36,19 +37,21 @@ def activate_condor_submit(config_name):
 
 # ---------------------------------------------------------------------- #
 user_name      = 'vasco.gennari'
-condor_memory  = 5
-condor_disk    = 5
-condor_threads = 16
+condor_memory  = 10 #3
+condor_disk    = 5  #1
+condor_threads = 16 #1
 condor_executable_path = '/home/vasco.gennari/.conda/envs/icarogw_env/bin/python'
 condor_executable_file = '/home/vasco.gennari/icarogw_pipeline/icarogw_pipeline/icarogw_runner.py'
 
 # Set the specific directory for the runs
-directory    = 'configs'
-subdirectory = 'simulation_evolving_population'
+directory    = '/home/vasco.gennari/icarogw_pipeline/config_files'
+subdirectory = 'tmp'
 # ---------------------------------------------------------------------- #
 
-if not os.path.exists('submission_files'): os.makedirs('submission_files')
-if not os.path.exists('condor_files'):     os.makedirs('condor_files')
+sub_path = os.path.join(directory, 'submission_files')
+con_path = os.path.join(directory, 'condor_files')
+if not os.path.exists(sub_path): os.makedirs(sub_path)
+if not os.path.exists(con_path): os.makedirs(con_path)
 
 if not (subdirectory == ''): final_path = os.path.join(directory, subdirectory)
 else:                        final_path = directory
@@ -60,4 +63,4 @@ for config in config_list:
     config_path = os.path.join(configs_path, config)
     activate_condor_submit(config_path)
 
-print('\nThe config files in {configs_path} are running in detached condor jobs. Good luck!'.format(configs_path=configs_path))
+print('\nThe config files in {configs_path} are running in detached condor jobs. Good luck!'.format(configs_path = configs_path))
