3c371b96dd00f5187509ba46d4b2d2a0c7a79664
Vasco Gennari <vasco.gennari@gmail.com>
2025-12-05 12:04:57 +0100

    minors snr

--- icarogw_pipeline/simulations/snr_computation.py
+++ icarogw_pipeline/simulations/snr_computation.py
@@ -517,7 +517,7 @@ try:
             waveform_params = {
                 
                 # Frequency range
-                'minf':                  1e-7,
+                'minf':                  1e-8,
                 'maxf':                  0.5,
                 
                 't0':                    t0[i],  # Reference epoch of coalescence, yr -- coalescence is at t0*yr + Deltat*s, Deltat in params
